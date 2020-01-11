26900e52911c747360051db7d5543e058352c663
Tabby Garcia <tabby.garcia1@gmail.com>
2020-01-11 12:43:07 -0600

    basic html,css,js.

---
+++ questions.js
@@ -0,0 +1,42 @@
+var score = []
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+  
+    {
+        title: "The condition in an if/else statement is enclosed with:",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "alerts"
+      },
+
+      {
+        title: "Arrays in Javascript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "alerts"
+      },
+
+      {
+        title: "String values must be enclosed within ____ when being assigned to variables:",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "alerts"
+      },
+
+      {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["javascript", "terminal/bash", "for loops", "console log"],
+        answer: "alerts"
+      },
+    ///etc.
+  ];
+//class notes for for loop
+  //for(var count=0; count<arr.length; count)
+  //var ul=document.createelement("ul")
+  //var title=arr[count].title;
+  //var choives=arr[count].choice;
+  //for (var count b=0; countb<choices.length);
+  //create li tags
+  //ul.apend(li)
+   //main.append(ul);
---
+++ script.js
@@ -0,0 +1,10 @@
+
+const start = document.getElementById ("start");
+const quiz = document.getElementById ("quiz");
+const timer = document.getElementById ("timer");
+const counter =document.getElementById ("counter");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+
