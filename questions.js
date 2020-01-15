2c22d72022bb2b792117e93e2a1a9c84942c0b50
Tabby Garcia <tabby.garcia1@gmail.com>
2020-01-14 20:40:28 -0600

    trying to go to next question. got first question and possible answers to load. still need if else statements and timer

--- questions.js
+++ questions.js
@@ -1,36 +1,39 @@
-var score = []
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
+      question: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     },
   
     {
-        title: "The condition in an if/else statement is enclosed with:",
+        question: "The condition in an if/else statement is enclosed with:",
         choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
         answer: "alerts"
       },
 
       {
-        title: "Arrays in Javascript can be used to store:",
+        question: "Arrays in Javascript can be used to store:",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "alerts"
+        answer: "all of the above"
       },
 
       {
-        title: "String values must be enclosed within ____ when being assigned to variables:",
+        question: "String values must be enclosed within ____ when being assigned to variables:",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
         answer: "alerts"
       },
 
       {
-        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["javascript", "terminal/bash", "for loops", "console log"],
-        answer: "alerts"
+        answer: "console log"
       },
     ///etc.
   ];
+
+
+
+
 //class notes for for loop
   //for(var count=0; count<arr.length; count)
   //var ul=document.createelement("ul")
--- script.js
+++ script.js
@@ -1,10 +1,66 @@
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
 
-const start = document.getElementById ("start");
-const quiz = document.getElementById ("quiz");
-const timer = document.getElementById ("timer");
-const counter =document.getElementById ("counter");
+let shuffledQuestions, currentQuestionIndex
 
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+//something is up with the classlist!
+//before begining all the functions think about the workflow of each
+//starting the quiz;change game to quiz
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+
+    setNextQuestion()
+}
+
+//goes to next question
+function setNextQuestion() {
+    //resetState() when i removed questions started up
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+
+
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.choices.forEach(answer => {
+        var button = document.createElement('button')
+        var buttonArea = document.getElementById("answer-buttons")
+        button.classList.add('btn');
+        button.textContent = answer;
+        buttonArea.appendChild(button);   
+
+        
+    })
+}
+
+//   if (answer.correct)
+//              button.dataset.correct = answer.correct
+    
+//      button.addEventListener('click', selectAnswer)
+//     answerButtonsElement.appendChild(startButton)
+
+// function selectAnswer(event) {
+// answerButtonsElement.appendChild(startButton)
+
+// }
+//MAYBE USE?
+//quizContainer.innerHTML = output.join('');
+
+
+
+
 
